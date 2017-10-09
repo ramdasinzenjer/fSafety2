ea099491ee282eab1eba2abcc69350b66bac6216
ramdas <ramdasinfoinzenjer@gmail.com>
2017-10-09 14:13:54 +0530

    change address

--- fSafety/src/main/java/srt/inz/connectors/Constants.java
+++ fSafety/src/main/java/srt/inz/connectors/Constants.java
@@ -8,7 +8,7 @@ public interface Constants {
 
     //Urls
    // String BASE_URL="http://192.168.1.10/Fire_safety/android_api/";
-    String BASE_URL="https://inzenjerfire.000webhostapp.com/?dir=android_api/";
+    String BASE_URL="https://inzenjerfire.000webhostapp.com/android_api/";
         
     String LOGIN_URL=BASE_URL+"mlogin.php?";   
     String NOTIFICATIONFETCH_URL=BASE_URL+"mNotifications.php?";  
